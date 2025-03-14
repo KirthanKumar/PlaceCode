ab4a355adc820626c79917f9eccafeb241e4ddb0
skirthankumar <skirthankumar13579@gmail.com>
2025-03-15 00:12:05 +0530

    Initialized and connected frontend and backend

---
+++ backend/server.js
@@ -0,0 +1,29 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+const app = express();
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+
+// Sample route
+app.get("/", (req, res) => {
+  res.send("Backend is running...");
+});
+
+// Connect to MongoDB
+const PORT = process.env.PORT || 5000;
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to MongoDB");
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+  })
+  .catch((error) => console.log(error.message));
